3a498dbb72f13a66f183340d4fbf657cb047c946
Aditya Neve <aditya.neve@gmail.com>
2022-03-11 00:14:05 +0530

    fetchData method added

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import DataTable from './Components/DataTable/DataTable';
+
+function App() {
+    const [adminData, setAdminData] = useState([]);
+    console.log('adminData:', adminData);
+
+    const fetchData = () => {
+        axios
+            .get(
+                `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json`
+            )
+            .then((response) => {
+                setAdminData(response.data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className='App'>
+            <DataTable adminData={adminData} setAdminData={setAdminData} />
+        </div>
+    );
+}
+
+export default App;
